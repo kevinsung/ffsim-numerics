c2effddb15412f112a325afa35329f0ab5a286ef
Kevin J. Sung <kevjsung@umich.edu>
2025-03-20 04:10:25 -0500

    update script

--- scripts/run/lucj_cobyqa_parallel/n2_6-31g_10e16o.py
+++ scripts/run/lucj_cobyqa_parallel/n2_6-31g_10e16o.py
@@ -27,7 +27,7 @@ logging.basicConfig(
 DATA_ROOT = Path(os.environ.get("FFSIM_NUMERICS_DATA_ROOT", "data"))
 DATA_DIR = DATA_ROOT / os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 MOLECULES_CATALOG_DIR = Path(os.environ.get("MOLECULES_CATALOG_DIR"))
-MAX_PROCESSES = 1
+MAX_PROCESSES = 72
 OVERWRITE = True
 
 molecule_name = "n2"
@@ -48,7 +48,7 @@ n_reps_range = [
     4,
     6,
 ]
-maxiter = 1000
+maxiter = 50
 ftol = 1e-12
 gtol = 1e-5
 
